eeabd014c997599db06ef1fb741eef64482d9e27
Hector Llorens <hectorlm1983@gmail.com>
2025-08-04 09:02:08 +0200

    even better server status

--- server_status.sh
+++ server_status.sh
@@ -3,5 +3,9 @@ if [ -f "server.pid" ] && ps -p $(cat server.pid) > /dev/null; then
     echo "Server is running with PID $(cat server.pid)."
     ps -p $(cat server.pid)
 else
-    echo "Server is not running."
+    if pgrep -f "python3 -m http.server 8000"; then
+        echo "($pwd)/server.pid is not running but a python http.server is running at port 8000."
+    else
+        echo "ERROR: Server is not running."
+    fi
 fi
--- stop_server.sh
+++ stop_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 kill $(cat server.pid)
+echo "Killed server (pid:$(cat server.pid)) with $0)
+rm -rf server.pid
